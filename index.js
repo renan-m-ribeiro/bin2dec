f8e22f9e4eacb7b18efd6a656afc2fc0485ce5a9
renan-m-ribeiro <renan.mendes013@gmail.com>
2022-06-09 14:53:25 -0300

    add index.js

---
+++ index.js
@@ -0,0 +1,40 @@
+window.onload = function () {
+
+    const input = document.querySelector('#inputBinary')
+    
+    console.log(input);
+    
+    input.addEventListener('keyup', () => {
+        const inputValue = getInputValue('#inputBinary').split('')
+        const decimalNumber = multiplyArrays(inputValue, createCalcArray(inputValue))
+                              .reduce((acc,current) => acc+current)
+        
+        console.log(decimalNumber);
+    })
+    
+    
+    const multiplyArrays = (array1 , array2) => {
+        let result = []
+        for( i = 0; i < array1.length ; i++ ) {
+            result.push(array1[i]*array2[i]) 
+        }
+        return result
+    }
+    
+    const createCalcArray = (element) => {
+        let x = 1
+        let array = []
+        for (let index = 0; index < element.length; index++) {
+            array[index] = x
+            x = x * 2
+        }
+        return array.reverse()
+    }
+
+    const getInputValue = (element) => {
+        let input = document.querySelector(element).value
+        return input;
+    }
+
+
+}
